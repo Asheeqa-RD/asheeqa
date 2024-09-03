5fd1d39b61dbe0504c7e165764d55d517a0729b4
Asheeqa-RD <asheeqakp123@gmail.com>
2024-09-03 10:55:47 +0530

    commit

---
+++ src/TimeIntervalSum.java
@@ -0,0 +1,63 @@
+
+import java.util.Scanner;
+
+
+public class TimeIntervalSum {
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Get the first time interval from the user
+        System.out.println("Enter the first time interval:");
+        System.out.print("Hours: ");
+        int hours1 = scanner.nextInt();
+        System.out.print("Minutes: ");
+        int minutes1 = scanner.nextInt();
+        System.out.print("Seconds: ");
+        int seconds1 = scanner.nextInt();
+
+        // Get the second time interval from the user
+        System.out.println("Enter the second time interval:");
+        System.out.print("Hours: ");
+        int hours2 = scanner.nextInt();
+        System.out.print("Minutes: ");
+        int minutes2 = scanner.nextInt();
+        System.out.print("Seconds: ");
+        int seconds2 = scanner.nextInt();
+
+        // Calculate the sum of the two time intervals
+        TimeInterval sum = addTimeIntervals(hours1, minutes1, seconds1, hours2, minutes2, seconds2);
+
+        // Display the result
+        System.out.println("Sum of time intervals: " + sum.hours + " hours, " + sum.minutes + " minutes, " + sum.seconds + " seconds");
+
+        scanner.close();
+    }
+
+    // Method to add two time intervals
+    public static TimeInterval addTimeIntervals(int h1, int m1, int s1, int h2, int m2, int s2) {
+        int totalSeconds1 = h1 * 3600 + m1 * 60 + s1;
+        int totalSeconds2 = h2 * 3600 + m2 * 60 + s2;
+        int totalSeconds = totalSeconds1 + totalSeconds2;
+
+        int hours = totalSeconds / 3600;
+        totalSeconds %= 3600;
+        int minutes = totalSeconds / 60;
+        int seconds = totalSeconds % 60;
+
+        return new TimeInterval(hours, minutes, seconds);
+    }
+
+    // Helper class to store time interval
+    static class TimeInterval {
+        int hours;
+        int minutes;
+        int seconds;
+
+        TimeInterval(int hours, int minutes, int seconds) {
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
+        }
+    }
+}
+
